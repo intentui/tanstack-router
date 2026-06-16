e144d7a8265316da5de7dab3cd5b94a78aee005e
Your Name <irsyad@parsinta.com>
2026-06-16 13:52:04 +0700

    bump

--- src/lib/primitive.ts
+++ src/lib/primitive.ts
@@ -1,13 +1,7 @@
+"use client"
+
 import { composeRenderProps } from "react-aria-components/composeRenderProps"
 import { type ClassNameValue, twMerge } from "tailwind-merge"
-
-/** @deprecated Use cx */
-export function composeTailwindRenderProps<T>(
-  className: string | ((v: T) => string) | undefined,
-  tailwind: ClassNameValue,
-): string | ((v: T) => string) {
-  return composeRenderProps(className, (className) => twMerge(tailwind, className))
-}
 
 type Render<T> = string | ((v: T) => string) | undefined
 
